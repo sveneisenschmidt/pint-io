aa52c44df3ff5fb7b2283e0503c3dbb1192b24d5
fate <sven.eisenschmidt@gmail.com>
2011-02-21 14:55:15 +0100

    updated example

--- lib/example/script.php
+++ lib/example/script.php
@@ -1,7 +1,19 @@
 <?php
 
-
 header("HTTP/1.0 404 Not Found");
 
-print '<h1>Not Found!</h1>';
-print '<p>Just kidding, playing around with some headers.</p>';
+// header("Content-type: application/json");
+?>
+
+<h1>Not Found!</h1>
+<p>Just kidding, playing around with some headers.</p>
+
+$_SERVER:
+<pre>
+<?php print_r($_SERVER); ?>
+</pre>
+
+$_GET:
+<pre>
+<?php print_r($_GET); ?>
+</pre>
